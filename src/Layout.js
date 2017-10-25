2131aca9bbef1ad8a7209d2848dd9981f3241b8b
RohanGada <gadarohan17@gmail.com>
2017-10-25 12:29:26 +0530

    testing config

---
+++ src/Layout.js
@@ -0,0 +1,13 @@
+import {Component} from 'react'
+class Layout extends Component {
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return (
+            <div className='App'>
+                
+            </div>
+        );
+    }
+}
